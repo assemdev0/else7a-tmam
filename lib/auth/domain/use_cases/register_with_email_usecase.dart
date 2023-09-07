94308af41209a0eeb6eb90923b75f03e5dea253f
Assem Ashraf <assemdev0@gmail.com>
2023-09-08 02:31:48 +0300

    add domain, data layer

---
+++ lib/auth/domain/use_cases/register_with_email_usecase.dart
@@ -0,0 +1,34 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import '../repositories/base_auth_repository.dart';
+import '/core/usecase/base_usecase.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../../core/error/failures.dart';
+
+class LoginWithEmailUseCase extends BaseUseCase<User, AuthParams> {
+  final BaseAuthRepository _baseAuthRepository;
+
+  LoginWithEmailUseCase(this._baseAuthRepository);
+
+  @override
+  Future<Either<Failure, User>> call(AuthParams params) async {
+    return await _baseAuthRepository.loginWithEmail(params);
+  }
+}
+
+class AuthParams extends Equatable {
+  final String email;
+  final String password;
+
+  const AuthParams({
+    required this.email,
+    required this.password,
+  });
+
+  @override
+  List<Object> get props => [
+        email,
+        password,
+      ];
+}
